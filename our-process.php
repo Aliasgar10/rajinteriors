99e4a51b656acf9d74956912da903e9d320532b7
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-04 13:31:46 +0530

    A86

--- our-process.php
+++ our-process.php
@@ -220,7 +220,7 @@
                                                 </div>
                                             </div>
                                         </section>
-                                        <div style="padding-bottom: 150px;" class="elementor-image">
+                                        <div style="padding-bottom: 150px; justify-content: center;" class="elementor-image">
                                             <img src="upload/workflow.jpg" class="attachment-large size-large" alt="" ></div>
                                         </div>
                                         <!-- <section data-id="79f616f" class="elementor-element elementor-element-79f616f elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}" data-element_type="section">
