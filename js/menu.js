06db90fd67dc4b8e749abb19f235eb952d52ab35
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-06 11:16:13 +0530

    s118

--- js/menu.js
+++ js/menu.js
@@ -4,42 +4,64 @@
 var $ = jQuery.noConflict();
 
 $(document).ready(function($) {
-	"use strict";
+    "use strict";
 
+    /*-------------------------------------------------*/
+    /* =  Mobile Menu (Three Dots Icon)
+    /*-------------------------------------------------*/
+    // Show the three-dots icon on mobile screens
+    function toggleMobileMenu() {
+        if ($(window).width() <= 768) {
+            $("#three-dots-menu").show(); // Show the three dots icon
+            $("#menu_border_wrapper select").hide(); // Hide the select box (we will use it for mobile)
+        } else {
+            $("#three-dots-menu").hide(); // Hide the three dots icon
+            $("#menu_border_wrapper select").show(); // Show the select box for larger screens
+        }
+    }
 
-	/*-------------------------------------------------*/
-	/* =  Main Nav 
-	/*-------------------------------------------------*/
+    // Initial check
+    toggleMobileMenu();
 
+    // Check on window resize to adjust the menu for mobile
+    $(window).resize(function() {
+        toggleMobileMenu();
+    });
 
-	/*-------------------------------------------------*/
-	/* =  Mobile Menu
-	/*-------------------------------------------------*/
-	// Create the dropdown base
+    // Toggle mobile menu when the three dots icon is clicked
+    $("#three-dots-menu").click(function() {
+        $("#mobile-menu").toggle(); // Show/Hide the mobile menu
+    });
+
+    // When a menu item is clicked, hide the mobile menu
+    $("#mobile-menu a").click(function() {
+        $("#mobile-menu").hide();
+    });
+
+    /*-------------------------------------------------*/
+    /* =  Dropdown Menu (Desktop Mode)
+    /*-------------------------------------------------*/
+    // Create the dropdown base
     $("<select />").appendTo("#menu_border_wrapper");
-    
+
     // Create default option "Go to..."
     $("<option />", {
-		"selected": "selected",
-		"value"   : "",
-		"text"    : "Menu"
+        "selected": "selected",
+        "value"   : "",
+        "text"    : "Menu"
     }).appendTo("#menu_border_wrapper select");
-    
+
     // Populate dropdown with menu items
     $(".nav a").each(function() {
-		var el = $(this);
-		$("<option />", {
-			"value"   : el.attr("href"),
-			"text"    : el.text()
-		}).appendTo("#menu_border_wrapper select");
+        var el = $(this);
+        $("<option />", {
+            "value"   : el.attr("href"),
+            "text"    : el.text()
+        }).appendTo("#menu_border_wrapper select");
     });
 
+    // Redirect to the selected menu item
     $("#menu_border_wrapper select").change(function() {
-		window.location = $(this).find("option:selected").val();
+        window.location = $(this).find("option:selected").val();
     });
-	
-
-
-
-
 });
