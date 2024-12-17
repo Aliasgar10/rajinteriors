861e30008cba3dd74f848a55f012d91d9a106790
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-17 12:01:00 +0530

    s316

--- inc_files/gallary.php
+++ inc_files/gallary.php
@@ -1,3 +1,11 @@
+<style>
+    @media screen and (max-width: 520px) {
+        #fb4c59e{
+            margin-top: 20vh;
+        }
+
+    }
+</style>
 <section data-id="fb4c59e" class="elementor-element elementor-element-fb4c59e animated fadeInUp animated-fast elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-invisible elementor-section elementor-top-section" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:200}" data-element_type="section">
     <div class="elementor-widget-container">
         <h2 class="elementor-heading-title elementor-size-default">Gallery</h2>
