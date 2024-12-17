44e9cc2c6e2c12ee2ec3784e2ed0f1f5a643b1b2
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-17 11:10:15 +0530

    s307

--- inc_files/header2.php
+++ inc_files/header2.php
@@ -23,32 +23,121 @@
                         font-size: 18px !important;
                     }
                 </style>
+                <!-- Desktop Menu -->
                 <div id="nav_wrapper">
                     <div class="nav_wrapper_inner">
                         <div id="menu_border_wrapper">
                             <div class="menu-main-menu-container">
                                 <ul id="main_menu" class="nav">
-                                    <li class="#"><a href="index.php">Home</a>
-                                        
-                                    <li class="#"><a href="#">TVS</a>
-                                        
-                                    </li>
-                                    <li class="#"><a href="gallery.php">Gallery</a>
-                                        
-                                    </li>
-                                    <li class="#"><a href="our-process.php">Services</a>
-                                        
-                                    </li>
-                                    <li class="#"><a href="contact-1.php">Contact</a>
-                                        
-                                    </li>
-                                    
+                                    <li class="#"><a href="index.php">Home</a></li>       
+                                    <li class="#"><a href="#">TVS</a></li>
+                                    <li class="#"><a href="gallery.php">Gallery</a></li>
+                                    <li class="#"><a href="our-process.php">Services</a></li>
+                                    <li class="#"><a href="contact-1.php">Contact</a></li>
                                 </ul>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <!-- Mobile Menu (Hamburger Icon) -->
+                <div id="mobile_menu_wrapper" style="display: none; color: grey;">
+                    <button id="hamburger_icon" style="color: #b8b8b8;">&#9776;</button>
+                    <div id="mobile_menu" style="display: none;">
+                        <ul id="mobile_menu_items">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">TVS</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="our-process.php">Services</a></li>
+                            <li><a href="contact-1.php">Contact</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    /* #logo_wrapper{
+        width: 15rem !important;
+        height: 100% !important;
+    }
+    #logo_wrapper img{
+        width: 100% !important;
+        height: 100% !important;
+    } */
+    
+    @media screen and (max-width: 480px) {
+        #menu_border_wrapper {
+            width: 140px !important;
+        }
+        #logo_wrapper {
+            float: left !important;
+            width: 0px !important;
+        }
+        #menu_wrapper {
+            margin-top: 12px !important;
+        }
+    }
+
+    /* Desktop Styles */
+    #menu_wrapper #nav_wrapper {
+        display: block;
+    }
+
+    #mobile_menu_wrapper {
+        display: none;
+    }
+    #main_menu{
+        margin-top:13px;
+    }
+    /* Mobile Styles */
+    @media only screen and (max-width: 520px) {
+        #menu_wrapper #nav_wrapper {
+            display: none; /* Hide the desktop menu */
+        }
+
+        #mobile_menu_wrapper {
+            display: block; /* Show the hamburger icon */
+        }
+
+        #mobile_menu {
+            display: none;  /* Mobile menu hidden initially */
+            background-color: beige;
+            padding: 10px;
+            position: absolute; /* Position the menu above other content */
+            top: 60px; /* Adjust this to position the menu correctly below the hamburger */
+            left: 0;
+            width: 100%;
+            z-index: 1000;  /* Ensure the mobile menu appears above other elements */
+            margin: 1px 5px 0px -10px;
+            border-radius: 10px;
+        
+        }
+
+        #mobile_menu ul {
+            list-style-type: none;
+            padding-left: 0;
+        }
+
+        #mobile_menu li {
+            margin: 10px 0;
+        }
+
+        #mobile_menu a {
+            text-decoration: none;
+            color: #333;
+            font-size: 18px;
+            padding: 10px;
+        }
+
+        /* Optional: Styling for the hamburger icon */
+        #hamburger_icon {
+            font-size: 30px;
+            background: none;
+            border: none;
+            cursor: pointer;
+        }
+    }
+
+</style>
