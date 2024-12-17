4938d6c3b75551427b69f4bf45307b501dd2b05a
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-17 16:55:19 +0530

    s344

--- inc_files/header3.php
+++ inc_files/header3.php
@@ -16,14 +16,14 @@
                 <div id="logo_normal" class="logo_container">
                     <div class="logo_align">
                         <a id="custom_logo" class="logo_wrapper hidden" href="index.php">
-                            <img src="imagg/tv.png" alt="" width="250" height="50">
+                            <img src="imagg/tv.png" alt="" width="70" height="70">
                         </a>
                     </div>
                 </div>
                 <div id="logo_transparent" class="logo_container">
                     <div class="logo_align">
                         <a id="custom_logo_transparent" class="logo_wrapper default" href="index.php">
-                            <img src="imagg/tv.png" alt="" width="250" height="50">
+                            <img src="imagg/tv.png" alt="" width="70" height="70">
                         </a>
                     </div>
                 </div>
