2cfb182a66ce2c45846fdb0a7ba175cfad3cdd28
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-18 18:25:25 +0530

    s390

--- inc_files/about_section.php
+++ inc_files/about_section.php
@@ -1,3 +1,9 @@
+<style>
+    #b3f72b28{
+        font-family:'inc_files/Operetta52-Bold.otf'
+    }
+</style>
+
 <section data-id="3f72b28" class="elementor-element elementor-element-3f72b28 elementor-section-height-min-height elementor-section-content-top elementor-section-items-top elementor-section-boxed elementor-section-height-default elementor-section elementor-top-section" data-element_type="section">
     <div class="elementor-container elementor-column-gap-default">
         <!-- Change from row to stacked vertical layout -->
@@ -8,8 +14,8 @@
                     <div data-id="c3053f6" class="elementor-element elementor-element-c3053f6 elementor-widget elementor-widget-text-editor" data-element_type="text-editor.default">
                         <div class="elementor-widget-container">
                             <div class="elementor-text-editor elementor-clearfix">
-                                <h4>About Us</h4>
-                                <h2 class="elementor-heading-title elementor-size-default">Welcome to Raj Interiors</h2>
+                                <!-- <h4>About Us</h4> -->
+                                <h2 class="elementor-heading-title elementor-size-default" id="b3f72b28">Welcome to Raj Interiors</h2>
                                 <p style="text-align: justify;">
                                     We are a team of experienced Interior Designers and skilled craftsmen dedicated to transforming your spaces into stunning realities. With over 10 years of serving satisfied customers, we have established ourselves as a trusted name in the interior design industry.<br><br>
                                     Our journey began in 2013, with a passion for creating beautiful and functional spaces. Over the years, we have honed our skills and expertise to deliver exceptional interior design solutions that exceed our clients' expectations.<br><br>
