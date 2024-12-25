d69243818cb61fd5494f47a468547669e0fd11dc
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-25 16:38:04 +0530

    s493

--- tvs.php
+++ tvs.php
@@ -146,7 +146,7 @@
                                                                                         }
                                                                                         .popup-content {
                                                                                             position: absolute;
-                                                                                            top: 63% !important;
+                                                                                            /* top: 63% !important; */
                                                                                             height: 713px !important;
                                                                                         }
                                                                                         .child-category-link {
