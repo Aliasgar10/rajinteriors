657280c3d9c4fb4b996c0072a1b99b877eb208c1
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-30 13:56:21 +0530

    s519

--- insert_user.php
+++ insert_user.php
@@ -1,23 +1,23 @@
 <?php
-    error_reporting(E_ALL);
-    // Display errors on the screen
-    ini_set('display_errors', 1);
-    // Log errors to a file (optional)
-    ini_set('log_errors', 1);
-// echo "hello";
+
 // Database configuration
-$host = 'localhost';
-$db_name = 'rajinteriors';
-$db_user = 'rajinteriors';
-$db_password = '7ku~3AksgI75Edzrp';
+$host = "localhost";
+$username = "rajinteriors";
+$password = "7ku~3AksgI75Edzrp";
+$database = "rajinteriors";
 
-// Create a connection
-$conn = new mysqli($host, $db_user, $db_password, $db_name);
+// Database connection
+$conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+error_reporting(E_ALL);
+// Display errors on the screen
+ini_set('display_errors', 1);
+// Log errors to a file (optional)
+ini_set('log_errors', 1);
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -25,10 +25,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user_name = $_POST['user_name'];
     $user_email = $_POST['user_email'];
     $message = $_POST['messages'];
+    
 echo $user_name;
 echo $user_email;
 echo $message;
-die;
+
     // Prepare SQL statement
     $stmt = $conn->prepare("INSERT INTO user_messages (user_name, user_email, messages) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $user_name, $user_email, $message);
